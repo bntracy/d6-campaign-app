dad7cefe766b18f57e3e9f85f51f330414835351
Beth Tracy <bntracy@gmail.com>
2024-03-11 14:44:43 -0500

    added data tables to database.sql

--- database.sql
+++ database.sql
@@ -8,3 +8,49 @@ CREATE TABLE "user" (
     "password" VARCHAR (1000) NOT NULL,
     "access_level" INTEGER NOT NULL DEFAULT 0
 );
+
+CREATE TABLE "character" (
+    "id" SERIAL PRIMARY KEY,
+    "character_name" VARCHAR(100) NOT NULL,
+    "user_id" INTEGER NOT NULL REFERENCES "user",
+    "species" VARCHAR(50),
+    "gender" VARCHAR(50),
+    "age" VARCHAR(50),
+    "height" VARCHAR(50),
+    "weight" VARCHAR(50),
+    "physical_description" VARCHAR(1000),
+    "dexterity_dice" INTEGER,
+    "dexterity_bonus" INTEGER,
+    "knowledge_dice" INTEGER,
+    "knowledge_bonus" INTEGER,
+    "mechanical_dice" INTEGER,
+    "mechanical_bonus" INTEGER,
+    "perception_dice" INTEGER,
+    "perception_bonus" INTEGER,
+    "strength_dice" INTEGER,
+    "strength_bonus" INTEGER,
+    "techincal_dice" INTEGER,
+    "technical_bonus" INTEGER,
+    "move" INTEGER,
+    "force_sensitive" BOOLEAN,
+    "force_points" INTEGER,
+    "dark_side_points" INTEGER,
+    "character_points" INTEGER,
+    "special_abilities" TEXT,
+    "equipment" TEXT,
+    "notes" TEXT,
+    "stunned" BOOLEAN NOT NULL DEFAULT false,
+    "wounded_1" BOOLEAN NOT NULL DEFAULT false,
+    "wounded_2" BOOLEAN NOT NULL DEFAULT false,
+    "incapacitated" BOOLEAN NOT NULL DEFAULT false,
+    "mortally_wounded" BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE "skills" (
+    "id" SERIAL PRIMARY KEY,
+    "character_id" INT NOT NULL REFERENCES "character",
+    "skill_name" VARCHAR(100),
+    "associated_attribute" VARCHAR(10) NOT NULL,
+    "skill_dice" INTEGER,
+    "skill_bonus" INTEGER
+);
