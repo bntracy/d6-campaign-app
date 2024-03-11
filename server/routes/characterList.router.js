10b7a54386ca158fe446fe43cc228f00fce06640
Beth Tracy <bntracy@gmail.com>
2024-03-11 18:24:08 -0500

    GET route for all characters added

--- server/routes/characterList.router.js
+++ server/routes/characterList.router.js
@@ -6,8 +6,20 @@ const {
 const router = express.Router();
 
 router.get('/', rejectUnauthenticated, (req, res) => {
-    console.log(req.user.access_level);
-    res.sendStatus(500);
+    // this request gets all characters and is for the gamemaster only
+    if (req.user.access_level === 1) {
+      const sqlQuery = `SELECT "user".username, character.id, character.character_name FROM "character"
+        JOIN "user" ON "user".id=character.user_id;`;
+      pool.query(sqlQuery).
+      then(response => {
+        res.send(response.rows);
+      }).catch(error => {
+        console.log('Error getting all characters', error);
+        res.sendStatus(500);
+      });
+    } else {
+      res.sendStatus(403);
+    }
 });
 
 module.exports = router;
