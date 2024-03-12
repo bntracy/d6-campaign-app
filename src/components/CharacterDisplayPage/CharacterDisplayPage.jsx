590ddcd35214ce6acc97127e89ff602f7b236a9b
Beth Tracy <bntracy@gmail.com>
2024-03-12 09:05:53 -0500

    added front-end auth check to character display page

--- src/components/CharacterDisplayPage/CharacterDisplayPage.jsx
+++ src/components/CharacterDisplayPage/CharacterDisplayPage.jsx
@@ -1,19 +1,24 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 function CharacterDisplayPage() {
     const dispatch = useDispatch();
     const { id } = useParams();
+    const user = useSelector(store => store.user);
 
     const fetchSingleCharacter = () => {
         // clear it out on page load
         dispatch({type: 'CLEAR_CHARACTER'});
+        // check to be sure the user should have access to this character
+        if (user.id === Number(id) || user.access_level === 1) {
+            console.log('show me the character');
+        }
     }
 
     useEffect(fetchSingleCharacter, []);
 
-    return <></>;
+    return <><p>No character data</p></>;
 }
 
 export default CharacterDisplayPage;
