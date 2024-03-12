3970daedc3df694a6c0e88780a34f8f961868982
Beth Tracy <bntracy@gmail.com>
2024-03-12 16:47:07 -0500

    AttributeEdit works

--- src/components/AttributeEdit/AttributeEdit.jsx
+++ src/components/AttributeEdit/AttributeEdit.jsx
@@ -1,5 +1,35 @@
-function AttributeEdit() {
-    return <></>;
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function AttributeEdit( {attribute, dice, bonus, setIsEditing} ) {
+    const dispatch = useDispatch();
+    const character = useSelector(store => store.character);
+    const [newDice, setNewDice] = useState(character[dice]);
+    const [newBonus, setNewBonus] = useState(character[bonus]);
+
+    const handleSave = event => {
+        event.preventDefault();
+        dispatch({
+            type: 'UPDATE_CHARACTER',
+            payload: {
+                ...character,
+                [dice]: Number(newDice),
+                [bonus]: Number(newBonus)
+            }
+        });
+        setIsEditing(false);
+    }
+
+    return <>
+        <form onSubmit={event => handleSave(event)}>
+            <h4>{attribute}:</h4>
+                <input type="number" value={newDice} onChange={event => setNewDice(event.target.value)}/>
+                D+
+                <input type="number" value={newBonus} onChange={event => setNewBonus(event.target.value)}/>
+            <button>Save</button>
+            <button onClick={()=>setIsEditing(false)}>Cancel</button>
+        </form>
+    </>;
 }
 
 export default AttributeEdit;
