90713e10a04eb8e3adb808f434de18982aad1132
Beth Tracy <bntracy@gmail.com>
2024-03-13 14:43:40 -0500

    got rid of routes I wasn't using in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,14 +9,9 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 
 import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
-import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
-import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
 
@@ -41,22 +36,13 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          {/* Visiting localhost:5173 will redirect to localhost:5173/home */}
-          <Redirect exact from="/" to="/home" />
-
-          {/* Visiting localhost:5173/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
+          {/* Visiting localhost:5173 will redirect to localhost:5173/character-selection */}
+          <Redirect exact from="/" to="/character-selection" />
 
           {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:5173/user will show the UserPage if the user is logged in.
+            Visiting localhost:5173/character-selection will show the CharacterSelectionPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
-            Even though it seems like they are different pages, the user is always on localhost:5173/user */}
+            Even though it seems like they are different pages, the user is always on localhost:5173/character-selection */}
           <ProtectedRoute
             // logged in shows CharacterSelectionPage else shows LoginPage
             exact
@@ -79,14 +65,6 @@ function App() {
             path="/new"
           >
             <NewCharacter />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
           </ProtectedRoute>
 
           <Route
@@ -117,26 +95,11 @@ function App() {
             }
           </Route>
 
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /character-selection page
-              <Redirect to="/character-selection" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
-            }
-          </Route>
-
           {/* If none of the other routes matched, we will show a 404. */}
           <Route>
             <h1>404</h1>
           </Route>
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
