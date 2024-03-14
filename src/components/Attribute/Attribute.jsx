cec408a53b5c424973022e2ebafe9e0446d01977
Beth Tracy <bntracy@gmail.com>
2024-03-14 10:12:29 -0500

    style changes to attribute componenet

--- src/components/Attribute/Attribute.jsx
+++ src/components/Attribute/Attribute.jsx
@@ -5,14 +5,17 @@ import AddSkill from '../AddSkill/AddSkill';
 import AttributeEdit from '../AttributeEdit/AttributeEdit';
 import Skill from '../Skill/Skill';
 
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
+
 function Attribute( {attribute, dice, bonus, skills} ) {
     const character = useSelector(store => store.character);
     const [isEditing, setIsEditing] = useState(false);
 
     return <>
         {isEditing ? <AttributeEdit attribute={attribute} dice={dice} bonus={bonus} setIsEditing={setIsEditing}/> : <>
-            <h4>{attribute}: {character[dice]}D{character[bonus] > 0 && <>+{character[bonus]}</>}</h4>
-            <button type="button" onClick={() => setIsEditing(true)}>Edit</button>
+            <h4>{attribute}: {character[dice]}D{character[bonus] > 0 && <>+{character[bonus]}</>}
+            <IconButton size="small" type="button" onClick={() => setIsEditing(true)}><EditIcon fontSize="inherit"/></IconButton></h4>
             {character[skills]?.length > 0 && <ul>{character[skills].map(skill => <Skill key={skill.id} skill={skill}/>)}</ul>}
             </>}
         <AddSkill attribute={attribute}/>
