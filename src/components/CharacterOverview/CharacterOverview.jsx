5ea36afed79adb72e4baee63148444728492b933
Beth Tracy <bntracy@gmail.com>
2024-03-14 09:37:01 -0500

    styling changes to character overview

--- src/components/CharacterOverview/CharacterOverview.jsx
+++ src/components/CharacterOverview/CharacterOverview.jsx
@@ -3,17 +3,21 @@ import { useSelector } from 'react-redux';
 
 import CharacterOverviewEdit from '../CharacterOverviewEdit/CharacterOverviewEdit';
 
+import './CharacterOverview.css'
+
 function CharacterOverview() {
     const character = useSelector(store => store.character);
     const [isEditing, setIsEditing] = useState(false);
 
     return <>
         {isEditing ? <CharacterOverviewEdit setIsEditing={setIsEditing} /> : <>
-        <div>Species: {character.species}</div>
-        <div>Gender: {character.gender}</div>
-        <div>Age: {character.age}</div>
-        <div>Height: {character.height}</div>
-        <div>Weight: {character.weight}</div>
+        <div className="flex-container">
+            <div>Species: {character.species}</div>
+            <div>Gender: {character.gender}</div>
+            <div>Age: {character.age}</div>
+            <div>Height: {character.height}</div>
+            <div>Weight: {character.weight}</div>
+        </div>
         <div>Physical Description: {character.physical_description}</div>
         <button type="button" onClick={() => setIsEditing(true)}>Edit</button></>}
     </>;
