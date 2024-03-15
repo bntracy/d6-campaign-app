a1278fe12663994a08d3e65e86d1089697e6c0ca
Beth Tracy <bntracy@gmail.com>
2024-03-15 10:04:04 -0500

    styled text component edit

--- src/components/TextComponentEdit/TextComponentEdit.jsx
+++ src/components/TextComponentEdit/TextComponentEdit.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 function TextComponentEdit( {setIsEditing, propertyToChange} ) {
     const dispatch = useDispatch();
     const character = useSelector(store => store.character);
@@ -20,9 +23,11 @@ function TextComponentEdit( {setIsEditing, propertyToChange} ) {
 
     return <>
         <form onSubmit={event => handleSave(event)}>
-            <textarea defaultValue={value} onChange={event => setValue(event.target.value)}></textarea>
-            <button type="submit">Save</button>
-            <button type="button" onClick={()=>setIsEditing(false)}>Cancel</button>
+            <TextField multiline defaultValue={value} onChange={event => setValue(event.target.value)} />
+            <div className="centering-div">
+                <Button variant="contained" type="submit">Save</Button>
+                <Button type="button" onClick={()=>setIsEditing(false)}>Cancel</Button>
+            </div>
         </form>
     </>;
 }
