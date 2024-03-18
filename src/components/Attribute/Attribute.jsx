b41fae803fc4b5fa66811480e9c81ece9e5cbba2
Beth Tracy <bntracy@gmail.com>
2024-03-18 11:20:55 -0500

    added roll buttons to attributes. they do nothing

--- src/components/Attribute/Attribute.jsx
+++ src/components/Attribute/Attribute.jsx
@@ -5,6 +5,7 @@ import AddSkill from '../AddSkill/AddSkill';
 import AttributeEdit from '../AttributeEdit/AttributeEdit';
 import Skill from '../Skill/Skill';
 
+import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
 
@@ -12,10 +13,15 @@ function Attribute( {attribute, dice, bonus, skills} ) {
     const character = useSelector(store => store.character);
     const [isEditing, setIsEditing] = useState(false);
 
+    const rollDice = (dice, bonus, label) => {
+        
+    }
+
     return <>
         {isEditing ? <AttributeEdit attribute={attribute} dice={dice} bonus={bonus} setIsEditing={setIsEditing}/> : <>
             <h4>{attribute}: {character[dice]}D{character[bonus] > 0 && <>+{character[bonus]}</>}
-            <IconButton size="small" sx={{mb: '4px'}} type="button" onClick={() => setIsEditing(true)}><EditIcon fontSize="inherit"/></IconButton></h4>
+            <IconButton size="small" sx={{mb: '4px'}} type="button" onClick={() => setIsEditing(true)}><EditIcon fontSize="inherit"/></IconButton>
+            <Button onClick={()=>rollDice(character[dice],character[bonus],attribute)}>Roll</Button></h4>
             {character[skills]?.length > 0 && <ul>{character[skills].map(skill => <Skill key={skill.id} skill={skill}/>)}</ul>}
             </>}
         <AddSkill attribute={attribute}/>
