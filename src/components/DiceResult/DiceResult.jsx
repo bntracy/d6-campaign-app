3a4cea995143d1efc7f2fd245ce3100d6d42bee4
Beth Tracy <bntracy@gmail.com>
2024-03-18 13:24:11 -0500

    roll result now displays nicely

--- src/components/DiceResult/DiceResult.jsx
+++ src/components/DiceResult/DiceResult.jsx
@@ -1,11 +1,18 @@
 import { useSelector } from "react-redux";
 
+import './DiceResult.css'
+
 function DiceResult() {
     const diceRoll = useSelector(store => store.diceRoll);
 
     return <>
         <hr/>
-        <p>Roll Result:{JSON.stringify(diceRoll)}</p>
+        <p>Roll Result:{diceRoll && <>
+            <span className="large">{diceRoll.sum}</span>
+            {diceRoll.diceArray.map(die => <>{die}+</>)}
+            {diceRoll.bonus}
+            <span className="roll-label">{diceRoll.label}</span>
+        </>}</p>
         <hr/>
     </>;
 }
