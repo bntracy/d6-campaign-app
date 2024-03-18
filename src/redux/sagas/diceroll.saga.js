b63bdca96890d48fafd54899869c5800538a42ec
Beth Tracy <bntracy@gmail.com>
2024-03-18 11:45:42 -0500

    dice roll now; still need wild die and to display results nicely

--- src/redux/sagas/diceroll.saga.js
+++ src/redux/sagas/diceroll.saga.js
@@ -1,7 +1,19 @@
 import { put, takeLatest } from 'redux-saga/effects';
 
 function* rollDice(action) {
-    yield put({type: 'SET_DICE_ROLL', payload: action.payload});
+    const dice = action.payload.dice;
+    const bonus = action.payload.bonus;
+    const label = action.payload.label;
+    const diceArray = [];
+    for (let i = 0; i < dice; i++) {
+        let randomRoll = Math.floor(Math.random() * 6) + 1;
+        diceArray.push(randomRoll);
+    }
+    let sum = 0;
+    diceArray.forEach(num => { sum += num });
+    sum += bonus;
+    let result = { diceArray, bonus, sum, label };
+    yield put({type: 'SET_DICE_ROLL', payload: result});
 }
 
 function* dicerollSaga() {
