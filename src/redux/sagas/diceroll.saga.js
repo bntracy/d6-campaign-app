648d1c9eefb1f8f75c9381572e42cfad898a449a
Beth Tracy <bntracy@gmail.com>
2024-03-18 12:48:45 -0500

    wild die logic added

--- src/redux/sagas/diceroll.saga.js
+++ src/redux/sagas/diceroll.saga.js
@@ -5,14 +5,26 @@ function* rollDice(action) {
     const bonus = action.payload.bonus;
     const label = action.payload.label;
     const diceArray = [];
-    for (let i = 0; i < dice; i++) {
+    let wildDieFlag = false;
+    // roll wild die
+    let wildDieRoll = Math.floor(Math.random() * 6) + 1;
+    if (wildDieRoll === 1) {
+        wildDieFlag = true; // if it is one, flag it
+    }
+    while (wildDieRoll === 6) { // if it is 6, add 6 to the array and roll again. Repeat if necessary until you don't get a 6
+        diceArray.push(wildDieRoll);
+        wildDieRoll = Math.floor(Math.random() * 6) + 1;
+    }
+    diceArray.push(wildDieRoll);
+    // roll nonwild dice
+    for (let i = 0; i < dice - 1; i++) {
         let randomRoll = Math.floor(Math.random() * 6) + 1;
         diceArray.push(randomRoll);
     }
     let sum = 0;
     diceArray.forEach(num => { sum += num });
     sum += bonus;
-    let result = { diceArray, bonus, sum, label };
+    let result = { diceArray, bonus, sum, label, wildDieFlag };
     yield put({type: 'SET_DICE_ROLL', payload: result});
 }
 
