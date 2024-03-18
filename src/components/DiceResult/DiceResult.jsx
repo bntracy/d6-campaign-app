a236755508324746917ff12a793ccc3dd4393e8f
Beth Tracy <bntracy@gmail.com>
2024-03-18 13:34:31 -0500

    minor styling changes

--- src/components/DiceResult/DiceResult.jsx
+++ src/components/DiceResult/DiceResult.jsx
@@ -7,13 +7,20 @@ function DiceResult() {
 
     return <>
         <hr/>
-        <p>Roll Result:{diceRoll && <>
-            <span className="large">{diceRoll.sum}</span>
-            {diceRoll.diceArray.map(die => <>{die}+</>)}
-            {diceRoll.bonus}
-            <span className="roll-label">{diceRoll.label}</span>
-            {diceRoll.wildDieFlag && <>Wild Die of <span className="wild-die">1</span></>}
-        </>}</p>
+        <div className="dice-container">
+            <div>
+                <p>Roll Result:{diceRoll && <>
+                    <span className="large">{diceRoll.sum}</span>
+                    {diceRoll.diceArray.map(die => <>{die}+</>)}
+                    {diceRoll.bonus}
+                    <span className="roll-label">{diceRoll.label}</span>
+                    {diceRoll.wildDieFlag && <>Wild Die of <span className="wild-die">1</span></>}
+                </>}</p>
+            </div>
+            <div>
+                <p>Add roll buttons here</p>
+            </div>
+        </div>
         <hr/>
     </>;
 }
