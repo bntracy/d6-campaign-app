47686021a6a00b8aa03c13156f282d5ee8009955
Beth Tracy <bntracy@gmail.com>
2024-03-19 15:23:48 -0500

    rolls in history now display nicely

--- src/components/RollHistory/RollHistory.jsx
+++ src/components/RollHistory/RollHistory.jsx
@@ -3,7 +3,17 @@ import { useSelector } from 'react-redux';
 function RollHistory() {
     const rollHistory = useSelector(store => store.rollHistory);
 
-    return <>{JSON.stringify(rollHistory)}</>;
+    const displayHistory = () => {
+        return <>{rollHistory.map((roll, index) => <div key={index}>
+            <p>Roll Result: <span className="large">{roll.sum}</span> {roll.diceArray.map((die, innerIndex) => <span key={innerIndex}>{die}+</span>)}
+            {roll.bonus}
+            <span className="roll-label">{roll.label}</span>
+            {roll.wildDieFlag && <>Wild Die of <span className="wild-die">1</span></>}
+            </p>
+        </div>)}</>;
+    }
+
+    return <>{rollHistory.length > 0 ? displayHistory() : <p>No rolls to display.</p>}</>;
 }
 
 export default RollHistory;
