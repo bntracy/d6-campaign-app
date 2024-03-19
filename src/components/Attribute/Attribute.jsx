44f3c76ceacfb531d90d9e0ec893d55495e8d020
Beth Tracy <bntracy@gmail.com>
2024-03-19 10:19:30 -0500

    added snackbar/validation to attribute and skill rolls

--- src/components/Attribute/Attribute.jsx
+++ src/components/Attribute/Attribute.jsx
@@ -8,17 +8,25 @@ import Skill from '../Skill/Skill';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
+import Slide from '@mui/material/Slide';
+import Snackbar from '@mui/material/Snackbar';
 
 function Attribute( {attribute, dice, bonus, skills} ) {
     const dispatch = useDispatch();
     const character = useSelector(store => store.character);
     const [isEditing, setIsEditing] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const rollDice = (dice, bonus, label) => {
+        if (dice < 1) {
+            setOpen(true);
+            return;
+        }
         dispatch({type: 'ROLL_DICE', payload: {dice, bonus, label}});
     }
 
     return <>
+        <Snackbar open={open} anchorOrigin={{vertical: 'top', horizontal: 'center'}} autoHideDuration={5000} onClose={()=>setOpen(false)} TransitionComponent={Slide} message="Cannot roll 0 or less dice"/>
         {isEditing ? <AttributeEdit attribute={attribute} dice={dice} bonus={bonus} setIsEditing={setIsEditing}/> : <>
             <h4>{attribute}: {character[dice]}D{character[bonus] > 0 && <>+{character[bonus]}</>}
             <IconButton size="small" sx={{mb: '4px'}} type="button" onClick={() => setIsEditing(true)}><EditIcon fontSize="inherit"/></IconButton>
--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -6,16 +6,24 @@ import SkillEdit from '../SkillEdit/SkillEdit';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
+import Slide from '@mui/material/Slide';
+import Snackbar from '@mui/material/Snackbar';
 
 function Skill( {skill} ) {
     const dispatch = useDispatch();
     const [isEditing, setIsEditing] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const rollDice = (dice, bonus, label) => {
+        if (dice < 1) {
+            setOpen(true);
+            return;
+        }
         dispatch({type: 'ROLL_DICE', payload: {dice, bonus, label}});
     }
 
     return <>
+        <Snackbar open={open} anchorOrigin={{vertical: 'top', horizontal: 'center'}} autoHideDuration={5000} onClose={()=>setOpen(false)} TransitionComponent={Slide} message="Cannot roll 0 or less dice"/>
         {isEditing ? <SkillEdit skill={skill} setIsEditing={setIsEditing}/> :
         <li>{skill.skill_name} {skill.skill_dice}D{skill.skill_bonus > 0 && <>+{skill.skill_bonus}</>}
         <IconButton size="small" sx={{mb: '2px', pt: 0}} type="button" onClick={() => setIsEditing(true)}><EditIcon fontSize="inherit"/></IconButton>
